312d3e6a30b6789f4bfbb4fd84ad9c252683af48
peizonghe <peizonghe@china-qizhi.com>
2016-08-09 15:24:09 +0800

    开发分支

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -1,6 +1,9 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'zh-CN', // 启用国际化支持
+	'sourceLanguage' => 'zh-CN', // 源代码采用中文
+	'timeZone' => 'Asia/Shanghai', // 设置时区
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
