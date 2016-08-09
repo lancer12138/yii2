d27117915239069c6e26b57b65b5e536d05f9783
peizonghe <peizonghe@china-qizhi.com>
2016-08-09 12:35:29 +0800

    创建博客项目

---
+++ Yii2Blog/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+//
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=test',
+  
+    'username' => 'qiaohu',
+    'password' => '2013Pwd07',
+    
+    
+    'charset' => 'utf8',
+];
